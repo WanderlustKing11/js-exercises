eaf4f577400a56a0869f8551d3899db5866bc128
Doug <wanderlustking11@gmail.com>
2023-05-02 10:01:18 -0400

    Slide In on Scroll background color shift

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -15,24 +15,33 @@ function debounce(func, wait = 20, immediate = true) {
 }
 
 const sliderImages = document.querySelectorAll('.slide-in');
+const bgColor = document.querySelector('html');
+const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
 
 function checkSlide(e) {
-  window.requestAnimationFrame(() => {
-    console.log(window.scrollY);
-    sliderImages.forEach(sldrImg => {
-      // half way through the image
-      const slideInAt = (window.scrollY + window.innerHeight) -  (sldrImg.height / 2);
-      // bottom of the image
-      const imageBottom = sldrImg.offsetTop + sldrImg.height;
-      const isHalfShown = slideInAt > sldrImg.offsetTop;
-      const isNotScrolledPast = window.scrollY < imageBottom;
-      if (isHalfShown && isNotScrolledPast) {
-          sldrImg.classList.add('active');
-      } else {
-          sldrImg.classList.remove('active');
-      }
-    });
+//   console.log(window.scrollY);
+  sliderImages.forEach(sldrImg => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) -   (sldrImg.height / 2);
+    // bottom of the image
+    const imageBottom = sldrImg.offsetTop + sldrImg.height;
+    const isHalfShown = slideInAt > sldrImg.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+        sldrImg.classList.add('active');
+    } else {
+        sldrImg.classList.remove('active');
+    }
   });
 }
 
+function backgroundShift() {
+  let scroll = window.scrollY;
+  console.log(scroll);
+//   let saturation = 100 - (scroll / maxScroll) * 100;
+  let light = (100 - (scroll / maxScroll) * 100) / 2;
+  bgColor.style.backgroundColor = `hsl(47, 100%, ${light}%)`;
+}
+
 window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener('scroll', debounce(backgroundShift));
